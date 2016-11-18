1c447b023f670cb721b37118a4dc94700dbab850
Stephan Tromer <stephan@stephantromer.nl>
2016-11-18 09:58:59 +0100

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Button button = new Button("Click here");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+
+        stage.setTitle("Hello World Application!");
+        stage.setScene(new Scene(root, 300, 300));
+        stage.show();
+    }
+}
