3b82360426178188c693f9bc1d09f1eeb5d73168
jiayinysj <jiayinysj@gmail.com>
2016-11-26 23:17:39 +0100

    Seconde try for aerodynamicist

---
+++ src/main/java/backend/Aerodynamicist.java
@@ -0,0 +1,12 @@
+package backend;
+
+public class Aerodynamicist extends Staff{
+
+    /**
+     *  Create a new aerodynamicisi
+     *
+     *  @param name the name of the staff member
+     *  @param salary the salary per race
+     */
+    public Aerodynamicist(String name, int salary) { super(name, salary);}
+}
