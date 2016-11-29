2cd863c587337831fdd98c60de5f0ce80029c464
jiayinysj <jiayinysj@gmail.com>
2016-11-29 16:14:41 +0100

    Add class engine

---
+++ src/main/java/backend/Engine.java
@@ -0,0 +1,109 @@
+package backend;
+
+public class Engine {
+    private int power;
+    private int drivability;
+    private int fuelConsumption;
+    private String name;
+
+    /**
+     * Create a new engine.
+     *
+     * @param power the power of the engine in hbp
+     * @param drivability the drivability of the engine (0-100)
+     * @param fuelConsumption the fuelconsumption of the engine in kg/h
+     * @param name the name of the engine
+     */
+
+    public Engine(int power, int drivability, int fuelConsumption, String name) {
+        this.power = power;
+        this.drivability = drivability;
+        this.fuelConsumption = fuelConsumption;
+        this.name = name;
+    }
+
+    /**
+     * Get the power of the engine.
+     *
+     * @return the power of the engine
+     */
+    public int getPower() {
+        return power;
+    }
+
+    /**
+     * Set the power of the engine.
+     *
+     * @param power the power of the engine
+     */
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    /**
+     * Get the drivability of the engine.
+     *
+     * @return the drivability of the engine
+     */
+    public int getDrivability() {
+        return drivability;
+    }
+
+    /**
+     * Set the drivability of the engine.
+     *
+     * @param drivability the drivabilty of the engine
+     */
+    public void setDrivability(int drivability) {
+        this.drivability = drivability;
+    }
+
+    /**
+     * Get the fuel consumption of the engine.
+     *
+     * @return the fuel consumption of the engine
+     */
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    /**
+     * Set the fuel consumption of the engine.
+     *
+     * @param fuelConsumption the fuel consumption
+     */
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    /**
+     * Get the name of the engine.
+     *
+     * @return the name of the engine
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the name of the engine.
+     *
+     * @param name the name of the engine
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        Engine engine = (Engine) other;
+
+        if (power != engine.power) return false;
+        if (drivability != engine.drivability) return false;
+        if (fuelConsumption != engine.fuelConsumption) return false;
+        return name != null ? name.equals(engine.name) : engine.name == null;
+    }
+}
---
+++ src/test/java/backend/EngineTest.java
@@ -0,0 +1,54 @@
+package backend;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EngineTest {
+    private Engine engine;
+    private Engine sameEngine;
+    private Engine otherEngine;
+
+    @Before
+    public void setUp() {
+        engine = new Engine(900, 70, 80, "Mercedes");
+        sameEngine = new Engine(900, 70, 80, "Mercedes");
+        otherEngine = new Engine (700, 50,100, "Honda");
+    }
+
+    @Test
+    public void testConstructor1() {
+        assertEquals(900, engine.getPower());
+    }
+
+    @Test
+    public void testConstructor2() {
+        assertEquals(50, engine.getDrivability());
+    }
+
+    @Test
+    public void testConstructor3() {
+        assertEquals(80, engine.getFuelConsumption());
+    }
+
+    @Test
+    public void testConstructor4() {
+        assertEquals("Mercedes", engine.getName());
+    }
+
+    @Test
+    public void testEquals_self() {
+        assertTrue(engine.equals(engine));
+    }
+
+    @Test
+    public void testEquals_same() {
+        assertTrue(engine.equals(sameEngine));
+    }
+
+    @Test
+    public void testEquals_other() {
+        assertFalse(engine.equals(otherEngine));
+    }
+}
