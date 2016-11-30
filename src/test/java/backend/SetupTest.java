95423e60c836f78eb467305cb9a9673f8d489cd9
mvanniekerk <matthijsvanniekerk@gmail.com>
2016-11-30 13:25:53 +0100

    Add tests for strategy and setup

--- src/test/java/backend/SetupTest.java
+++ src/test/java/backend/SetupTest.java
@@ -1,9 +1,43 @@
 package backend;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class SetupTest {
+    private Setup setup;
+    private Setup sameSetup;
+    private Setup otherSetup;
+
+    @Before
+    public void SetUp() {
+        setup = new Setup(Setup.LOW_RISK);
+        sameSetup = new Setup(Setup.LOW_RISK);
+        otherSetup = new Setup(Setup.MEDIUM_RISK);
+    }
+
+    @Test
+    public void riskTest() {
+        assertEquals(Setup.LOW_RISK, setup.getRisk());
+    }
+
+    @Test
+    public void sameEquals() {
+        assertEquals(setup, setup);
+    }
+
+    @Test
+    public void otherEquals() {
+        assertNotEquals(setup, otherSetup);
+    }
+
+    @Test
+    public void sameEqualsAttr() {
+        assertEquals(setup, sameSetup);
+    }
 
     @Test
     public void lowRiskTest(){
