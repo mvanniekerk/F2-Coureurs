6f630102fcf4cc88d8a5c896a6baf8ce9de26150
mvanniekerk <matthijsvanniekerk@gmail.com>
2016-12-06 15:13:03 +0100

    Fixed bugs with multiple controllers

--- src/main/java/frontend/controllers/HomeController.java
+++ src/main/java/frontend/controllers/HomeController.java
@@ -29,6 +29,11 @@ public class HomeController {
         stage.getScene().setRoot(root);
     }
 
+    /**
+     * Quits the game when the quit button is pressed.
+     *
+     * @param event the event that called this method
+     */
     @FXML
     public void quitGameAction(ActionEvent event) {
         Stage stage = (Stage) quitGame.getScene().getWindow();
--- src/main/java/frontend/controllers/PrepareRaceController.java
+++ src/main/java/frontend/controllers/PrepareRaceController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class PrepareRaceController {
+public class PrepareRaceController extends RaceController {
 
     @FXML
     private Button quitGame;
