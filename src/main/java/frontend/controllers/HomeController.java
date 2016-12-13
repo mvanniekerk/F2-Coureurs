f3c0f1e165ed68972ce6940a856db19cd59b3530
Alex Settels <a.j.settels@student.tudelft.nl>
2016-12-13 16:43:33 +0100

    made HomeController that opens the right screens on the right buttons

--- src/main/java/frontend/controllers/HomeController.java
+++ src/main/java/frontend/controllers/HomeController.java
@@ -13,7 +13,10 @@ public class HomeController {
     private Button buttonPrepareRace;
 
     @FXML
-    private Button quitGame;
+    private Button buttonChampionshipStandings;
+
+    @FXML
+    private Button buttonEditTeam;
 
     /**
      * Show the prepare race screen.
@@ -25,6 +28,34 @@ public class HomeController {
     public void prepareRaceAction(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/views/prepare-race.fxml"));
         Stage stage = (Stage) buttonPrepareRace.getScene().getWindow();
+
+        stage.getScene().setRoot(root);
+    }
+
+    /**
+     * Show the championship standings screen.
+     *
+     * @param event the event that called this method
+     * @throws Exception if view is not found
+     */
+    @FXML
+    public void championshipStandingsAction(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/standings.fxml"));
+        Stage stage = (Stage) buttonChampionshipStandings.getScene().getWindow();
+
+        stage.getScene().setRoot(root);
+    }
+
+    /**
+     * Show the edit team screen.
+     *
+     * @param event the event that called this method
+     * @throws Exception if view is not found
+     */
+    @FXML
+    public void editTeamAction(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/edit-team.fxml"));
+        Stage stage = (Stage) buttonEditTeam.getScene().getWindow();
 
         stage.getScene().setRoot(root);
     }
