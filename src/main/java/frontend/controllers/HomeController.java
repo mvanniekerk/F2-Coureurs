e4375286882baee9f09425b71e42d38b8890a14e
mvanniekerk <matthijsvanniekerk@gmail.com>
2016-12-14 17:40:16 +0100

    Remove unnecessary code

--- src/main/java/frontend/controllers/HomeController.java
+++ src/main/java/frontend/controllers/HomeController.java
@@ -3,48 +3,20 @@ package frontend.controllers;
 import backend.GameEngine;
 import backend.Season;
 import backend.Team;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 public class HomeController {
-
-    @FXML
-    private Button buttonPrepareRace;
-
-    @FXML
-    private Label teamName;
-
-    @FXML
-    private Label engineName;
-
-    @FXML
-    private Label firstDriverName;
-
-    @FXML
-    private Label secondDriverName;
-
-    @FXML
-    private Label strategistName;
-
-    @FXML
-    private Label aerodynamicistName;
-
-    @FXML
-    private Label mechanicName;
-
-    @FXML
-    private Label nextCircuit;
-
-    @FXML
-    private Label currentBudget;
-
+    @FXML private Label teamName;
+    @FXML private Label engineName;
+    @FXML private Label firstDriverName;
+    @FXML private Label secondDriverName;
+    @FXML private Label strategistName;
+    @FXML private Label aerodynamicistName;
+    @FXML private Label mechanicName;
+    @FXML private Label nextCircuit;
+    @FXML private Label currentBudget;
     private Season season;
-
 
     /**
      * Initialize the home screen with the correct values.
@@ -66,17 +38,4 @@ public class HomeController {
 
     }
 
-    /**
-     * Show the prepare race screen.
-     *
-     * @param event the event that called this method
-     * @throws Exception if view is not found
-     */
-    @FXML
-    public void prepareRaceAction(ActionEvent event) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/prepare-race.fxml"));
-        Stage stage = (Stage) buttonPrepareRace.getScene().getWindow();
-
-        stage.getScene().setRoot(root);
-    }
 }
