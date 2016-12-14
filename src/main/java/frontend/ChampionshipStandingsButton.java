c7d0d9ea0c908cb5da2633af49c02547e8e4384e
Alex Settels <a.j.settels@student.tudelft.nl>
2016-12-14 15:34:03 +0100

    Make ChampionshipStandingsButton

---
+++ src/main/java/frontend/ChampionshipStandingsButton.java
@@ -0,0 +1,37 @@
+package main.java.frontend;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChampionshipStandingsButton extends Button {
+
+
+    /**
+     * Constructs a new Championship Standings Button that loads the standings screen when clicked.
+     *
+     */
+    public ChampionshipStandingsButton() {
+        this.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/views/standings.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Node source = (Node) event.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+
+                stage.getScene().setRoot(root);
+            }
+        });
+    }
+}
