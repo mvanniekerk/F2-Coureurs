860414482bc13b65dc7f2a974908a17e38edd347
Stephan Tromer <stephan@stephantromer.nl>
2016-12-20 18:04:35 +0100

    Add table with race results
    For too me unknown reasons lambda's are not working, so that's why I used these large callback functions, which look really ugly in combination with checkstyle.

--- src/main/java/frontend/controllers/RaceController.java
+++ src/main/java/frontend/controllers/RaceController.java
@@ -2,62 +2,88 @@ package frontend.controllers;
 
 import backend.Driver;
 import backend.GameEngine;
+import backend.Race;
 import backend.Season;
-import backend.Team;
-import javafx.event.ActionEvent;
+import backend.Setup;
+import backend.Strategy;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-import java.io.InputStream;
-import java.util.Random;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 
 public class RaceController {
 
     @FXML
-    private Button startRace;
-
-    @FXML
-    private Label winningDriverLabel;
+    private TableView<Driver> resultsTable;
 
     private Season season;
 
+    /**
+     * Initialize the RaceController.
+     */
     @FXML
     public void initialize() {
         season = GameEngine.getInstance().getSeason();
-    }
 
-    /**
-     * Start the race.
-     *
-     * @param event the event that called this method
-     * @throws Exception if view is not found
-     */
-    @FXML
-    public void startRace(ActionEvent event) throws Exception {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream seasonStart = loader.getResourceAsStream("seasonStart.json");
-        Season season = Season.readFromJsonFile(seasonStart);
+        // Print test race
+        Race race = new Race(
+                new Setup(Setup.HIGH_RISK),
+                new Strategy(Strategy.HIGH_RISK),
+                "Monaco",
+                1
+        );
 
-        // Select random team
-        int random = new Random().nextInt(season.getTeams().size());
-        Team winningTeam = season.getTeams().get(random);
+        ObservableList<Driver> drivers =
+                FXCollections.observableArrayList(race.calculateRaceResult());
 
-        // Select first driver from the winning team
-        Driver winningDriver = winningTeam.getSecondDriver();
+        resultsTable.setItems(drivers);
 
-        Parent root = FXMLLoader.load(getClass().getResource("/views/race-result.fxml"));
-        Stage stage = (Stage)startRace.getScene().getWindow();
+        TableColumn<Driver, String> positionColumn = new TableColumn<>("#");
+        positionColumn.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<Driver, String>, ObservableValue<String>>() {
 
-        stage.getScene().setRoot(root);
+                @Override
+                public ObservableValue<String> call(
+                    TableColumn.CellDataFeatures<Driver, String> driverObject
+                ) {
+                    return new SimpleStringProperty(
+                        (resultsTable.getItems().indexOf(driverObject.getValue()) + 1) + ""
+                    );
+                }
+            }
+        );
 
-        // Get label and set it to the winning driver
-        winningDriverLabel = (Label) stage.getScene().lookup("#winningDriverLabel");
-        winningDriverLabel.setText("Race winner: " + winningDriver.getName());
-        winningDriverLabel.setTextFill(Color.BLACK);
+        TableColumn<Driver, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<Driver, String>, ObservableValue<String>>() {
+
+                @Override
+                public ObservableValue<String> call(
+                    TableColumn.CellDataFeatures<Driver, String> driverObject
+                ) {
+                    return new SimpleStringProperty(driverObject.getValue().getName());
+                }
+            }
+        );
+
+        TableColumn<Driver, String> scoreColumn = new TableColumn<>("Score");
+        scoreColumn.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<Driver, String>,
+                    ObservableValue<String>>() {
+
+                @Override
+                public ObservableValue<String> call(
+                    TableColumn.CellDataFeatures<Driver, String> driverObject
+                ) {
+                    return new SimpleStringProperty("" + driverObject.getValue().getScore());
+                }
+            }
+        );
+
+        resultsTable.getColumns().addAll(positionColumn, nameColumn, scoreColumn);
     }
 }
