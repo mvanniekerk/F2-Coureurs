bdf23f27a072123add26622e57f25bff0bc0feb9
jiayinysj <jiayinysj@gmail.com>
2017-01-15 21:45:41 +0100

    Update load-game controller

--- src/main/java/frontend/controllers/LoadGameController.java
+++ src/main/java/frontend/controllers/LoadGameController.java
@@ -5,84 +5,50 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+
+import java.io.File;
 import java.io.IOException;
 
 public class LoadGameController {
-
-    /**
-     * Load Game A
-     * @param event ActionEvent that the button experienced (presumably a button-press).
-     * @throws IOException throws if the json file can not be found
-     */
     @FXML
-    public void gameA(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
-        Parent root = loader.load();
-        HomeController controller = loader.getController();
-        controller.initialize("save1.json");
-
-        Node source = (Node) event.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
-
-        stage.getScene().setRoot(root);
-    }
-
-    /**
-     * Load Game B.
-     *
-     * @param event ActionEvent that the button experienced (presumably a button-press).
-     * @throws IOException throws if the json file can not be found
-     */
+    private Button a;
     @FXML
-    public void gameB(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
-        Parent root = loader.load();
-        HomeController controller = loader.getController();
-        controller.initialize("save2.json");
-
-        Node source = (Node) event.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
-
-        stage.getScene().setRoot(root);
-    }
-
-    /**
-     * Load Game C.
-     *
-     * @param event ActionEvent that the button experienced (presumably a button-press).
-     * @throws IOException throws if the json file can not be found
-     */
+    private Button b;
     @FXML
-    public void gameC(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
-        Parent root = loader.load();
-        HomeController controller = loader.getController();
-        controller.initialize("save3.json");
-
-        Node source = (Node) event.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
-
-        stage.getScene().setRoot(root);
-    }
-
-    /**
-     * Load Game D.
-     *
-     * @param event ActionEvent that the button experienced (presumably a button-press).
-     * @throws IOException throws if the json file can not be found
-     */
+    private Button c;
     @FXML
-    public void gameD(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
-        Parent root = loader.load();
-        HomeController controller = loader.getController();
-        controller.initialize("save4.json");
+    private Button d;
+    private String saveName;
 
-        Node source = (Node) event.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
+    public void game(ActionEvent event) throws IOException {
+        Button button = (Button) event.getSource();
 
-        stage.getScene().setRoot(root);
+        if (button == a) {
+            saveName = "save1.json";
+        } else if (button == b) {
+            saveName = "saveB.json";
+        } else if (button == c) {
+            saveName = "saveC.json";
+        } else if (button == d) {
+            saveName = "saveD.json";
+        }
+
+        File file = new File("saves/" + saveName);
+        if (file.exists()) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
+            Parent root = loader.load();
+            HomeController controller = loader.getController();
+            controller.initialize(saveName);
+
+            Node source = (Node) event.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+
+            stage.getScene().setRoot(root);
+        } else {
+            button.setStyle("-fx-background-color: rgba(255, 0, 0, 0.59)");
+        }
     }
 }
