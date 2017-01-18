2c4a2e483a8b6bd9102f3851537179f776fd0be2
jiayinysj <jiayinysj@gmail.com>
2017-01-18 12:02:09 +0100

    Save the file when the game is updated

--- src/main/java/frontend/ExitButton.java
+++ src/main/java/frontend/ExitButton.java
@@ -1,16 +1,23 @@
 package frontend;
 
+import backend.GameEngine;
+import backend.Season;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 
 public class ExitButton extends Button {
+    private Season season;
+    private String saveName;
 
     /**
      * Constructs a new Exit Button that closes the application when clicked.
      *
      */
     public ExitButton() {
+        season = GameEngine.getInstance().getSeason();
+        saveName = GameEngine.getInstance().getSaveName();
+        season.save(saveName);
         this.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
