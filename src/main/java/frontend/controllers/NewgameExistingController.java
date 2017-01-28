36906ad534ed16e9483ffa9c17ac089bfe279fd7
mvanniekerk <matthijsvanniekerk@gmail.com>
2017-01-28 12:06:04 +0100

    temp commit

---
+++ src/main/java/frontend/controllers/NewgameExistingController.java
@@ -0,0 +1,18 @@
+package frontend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class NewgameExistingController {
+    @FXML Pane ferrari;
+    @FXML Pane mercedes;
+    @FXML Pane redbull;
+    @FXML Pane mclaren;
+    @FXML Pane forceindia;
+    @FXML Pane haas;
+    @FXML Pane manor;
+    @FXML Pane renault;
+    @FXML Pane sauber;
+    @FXML Pane torrorosso;
+    @FXML Pane williams;
+}
