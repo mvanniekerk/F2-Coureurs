1b56769e6129d7179bcfff8183b019c9dc4f9b6b
mvanniekerk <matthijsvanniekerk@gmail.com>
2017-01-31 12:53:12 +0100

    Remove unused imports

--- src/main/java/frontend/controllers/HomeController.java
+++ src/main/java/frontend/controllers/HomeController.java
@@ -4,17 +4,10 @@ import backend.GameEngine;
 import backend.Season;
 import backend.Team;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HomeController {
     public static boolean trigger;
