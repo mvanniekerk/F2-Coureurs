89c5d43c5c269d6fa0453e14ed0c81effeb6159a
mvanniekerk <matthijsvanniekerk@gmail.com>
2017-01-31 11:49:21 +0100

    newGameExisting now works as expected

--- src/main/java/frontend/controllers/NewgameExistingController.java
+++ src/main/java/frontend/controllers/NewgameExistingController.java
@@ -1,9 +1,6 @@
 package frontend.controllers;
 
-import backend.GameEngine;
-import backend.Season;
-import backend.Team;
-import javafx.event.ActionEvent;
+import backend.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,94 +10,63 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NewgameExistingController {
     @FXML private Pane newgameExisting;
     @FXML private Pane ferrari;
+    @FXML private Pane mercedes;
+    @FXML private Pane redbull;
+    @FXML private Pane mclaren;
+    @FXML private Pane forceindia;
+    @FXML private Pane haas;
+    @FXML private Pane manor;
+    @FXML private Pane renault;
+    @FXML private Pane sauber;
+    @FXML private Pane tororosso;
+    @FXML private Pane williams;
+
 
     @FXML
     public void initialize() {
-        ferrari.setOnMouseClicked((MouseEvent event) -> buttonAction(0));
+        ferrari.setOnMouseClicked((MouseEvent event) -> buttonAction("Ferrari"));
+        mercedes.setOnMouseClicked((MouseEvent event) -> buttonAction("Mercedes"));
+        redbull.setOnMouseClicked((MouseEvent event) -> buttonAction("Red Bull"));
+        mclaren.setOnMouseClicked((MouseEvent event) -> buttonAction("McLaren"));
+        forceindia.setOnMouseClicked((MouseEvent event) -> buttonAction("Force India"));
+        haas.setOnMouseClicked((MouseEvent event) -> buttonAction("Haas"));
+        manor.setOnMouseClicked((MouseEvent event) -> buttonAction("Manor"));
+        renault.setOnMouseClicked((MouseEvent event) -> buttonAction("Renault"));
+        sauber.setOnMouseClicked((MouseEvent event) -> buttonAction("Sauber"));
+        tororosso.setOnMouseClicked((MouseEvent event) -> buttonAction("Toro Rosso"));
+        williams.setOnMouseClicked((MouseEvent event) -> buttonAction("Williams"));
 
     }
 
-    private void buttonAction(int teamNumber) {
+    private void buttonAction(String teamName) {
         new GameEngine.GameEngineBuilder("save1.json").build();
         List<Team> teams = GameEngine.getInstance().getSeason().getTeams();
-        teams.add(0, teams.remove(teamNumber));
+        List<String> teamstr = teams.stream().map((Team team) -> team.getName()).collect(Collectors.toList());
+        System.out.println(teamstr);
+        Team team = null;
+        try {
+            team = teams.stream()
+                    .filter((Team te) -> te.getName().equals(teamName))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        teams.remove(team);
+        teams.add(0, team);
 
         Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("/views/home.fxml"));
         } catch (IOException e) {
-            System.out.println("HELP!");
             e.printStackTrace();
         }
         Stage stage = (Stage) newgameExisting.getScene().getWindow();
         stage.getScene().setRoot(root);
-    }
-
-    @FXML
-    public void ferrari(ActionEvent event) throws IOException {
-        buttonAction(0);
-    }
-
-    @FXML
-    public void mercedes(ActionEvent event) throws IOException {
-        buttonAction(4);
-
-    }
-
-    @FXML
-    public void redbull(ActionEvent event) throws IOException {
-        buttonAction(6);
-
-    }
-
-    @FXML
-    public void mclaren(ActionEvent event) throws IOException {
-        buttonAction(3);
-
-    }
-
-    @FXML
-    public void forceindia(ActionEvent event) throws IOException {
-        buttonAction(1);
-
-    }
-
-    @FXML
-    public void haas(ActionEvent event) throws IOException {
-        buttonAction(2);
-    }
-
-    @FXML
-    public void manor(ActionEvent event) throws IOException {
-        buttonAction(5);
-
-    }
-
-    @FXML
-    public void renault(ActionEvent event) throws IOException {
-        buttonAction(7);
-
-    }
-
-    @FXML
-    public void sauber(ActionEvent event) throws IOException {
-        buttonAction(8);
-
-    }
-
-    @FXML
-    public void torrorosso(ActionEvent event) throws IOException {
-        buttonAction(9);
-
-    }
-
-    @FXML
-    public void williams(ActionEvent event) throws IOException {
-        buttonAction(10);
-
     }
 }
